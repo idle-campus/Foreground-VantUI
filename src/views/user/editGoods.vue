7d0d053ec89f3705276ac2f1ba6ca180c77aedfa
linsizao <linsizao@163.com>
2023-04-09 17:05:48 +0800

    chore: myGoods 页面交互刷新数据 & 路由跳转编辑页面修改

--- src/views/user/editGoods.vue
+++ src/views/user/editGoods.vue
@@ -1,36 +1,29 @@
 <template>
   <div>
-
-      <van-nav-bar
-      :title= this.title
+    <van-nav-bar
+      :title="this.title"
       left-text="返回"
       left-arrow
       @click-left="onClickLeft"
     />
 
-
-
     <van-form @submit="onSubmit">
-    <!-- 上传闲置物品图片 -->
-    <van-field required label="上传图片" center>
-      <template #input>
-        <van-uploader
-        :max-count="1"
-        :after-read="afterRead"
-        >
-        <van-image
-          fit="cover"
-          height="6rem"
-          :src="img_url+form.photo"
-          width="6rem"
-        />
-        </van-uploader>
-      </template>
-    </van-field>
-    <!-- 上传闲置物品图片end -->
+      <!-- 上传闲置物品图片 -->
+      <van-field required label="上传图片" center>
+        <template #input>
+          <van-uploader :max-count="1" :after-read="afterRead">
+            <van-image
+              fit="cover"
+              height="6rem"
+              :src="img_url + form.photo"
+              width="6rem"
+            />
+          </van-uploader>
+        </template>
+      </van-field>
+      <!-- 上传闲置物品图片end -->
 
-
-    <van-cell-group>
+      <van-cell-group>
         <van-field
           v-model="form.name"
           required
@@ -43,16 +36,19 @@
           required
           label="原价"
           placeholder="请输入原价"
-          :rules="[{ required: true },
-          { validator: priceValidator, message: '价格格式错误！最多两位小数'},]"
+          :rules="[
+            { required: true },
+            { validator: priceValidator, message: '价格格式错误！最多两位小数' }
+          ]"
         />
         <van-field
           v-model="form.sellPrice"
           required
           label="售价"
           placeholder="请输入售价"
-          :rules="[{ required: true },
-          { validator: priceValidator, message: '价格格式错误！最多两位小数'},
+          :rules="[
+            { required: true },
+            { validator: priceValidator, message: '价格格式错误！最多两位小数' }
           ]"
         />
         <van-field
@@ -81,187 +77,188 @@
           placeholder="选择闲置分类"
           @click="showPicker = true"
         />
-        <van-popup v-model="showPicker" round position="bottom" :style="{ height: '80%' }" >
+        <van-popup
+          v-model="showPicker"
+          round
+          position="bottom"
+          :style="{ height: '80%' }"
+        >
           <div id="goBack_title">
-            <van-button icon="arrow-left" type="primary" round size="small" color="#b4b4b4"
-                        @click="goBack"/>
+            <van-button
+              icon="arrow-left"
+              type="primary"
+              round
+              size="small"
+              color="#b4b4b4"
+              @click="goBack"
+            />
           </div>
           <van-row>
-          <!--      侧边导航条-->
-          <van-col span="6">
-            <van-sidebar v-model="activeKey">
-              <van-sidebar-item
-                v-for="category in goodsCategory"
-                :key="category.id" :title="category.name"
-                @click="tab_item(category.id)"
-              />
-            </van-sidebar>
-          </van-col>
-          <!--      侧边导航条end-->
-          <!--      商品分类详情-->
-          <van-col span="18">
-            <van-grid square :column-num="3" clickable id="grid">
-              <van-grid-item v-for="children in category_children" :key="children.id">
-                <van-image
-                  width="4rem"
-                  height="4rem"
-                  fit="cover"
-                  :src='img_url+children.icon'
-                  @click="goConfirm(children.id,children.name)"
+            <!--      侧边导航条-->
+            <van-col span="6">
+              <van-sidebar v-model="activeKey">
+                <van-sidebar-item
+                  v-for="category in goodsCategory"
+                  :key="category.id"
+                  :title="category.name"
+                  @click="tab_item(category.id)"
                 />
-                <span>{{ children.name }}</span>
-              </van-grid-item>
-            </van-grid>
-          </van-col>
-          <!--      商品分类详情end-->
-        </van-row>
+              </van-sidebar>
+            </van-col>
+            <!--      侧边导航条end-->
+            <!--      商品分类详情-->
+            <van-col span="18">
+              <van-grid square :column-num="3" clickable id="grid">
+                <van-grid-item
+                  v-for="children in category_children"
+                  :key="children.id"
+                >
+                  <van-image
+                    width="4rem"
+                    height="4rem"
+                    fit="cover"
+                    :src="img_url + children.icon"
+                    @click="goConfirm(children.id, children.name)"
+                  />
+                  <span>{{ children.name }}</span>
+                </van-grid-item>
+              </van-grid>
+            </van-col>
+            <!--      商品分类详情end-->
+          </van-row>
         </van-popup>
       </van-cell-group>
 
       <div style="margin: 16px">
-        <van-button round block type="info" native-type="submit" class="btn">更新发布</van-button>
+        <van-button round block type="info" native-type="submit" class="btn">
+          更新发布
+        </van-button>
       </div>
-
-
     </van-form>
-
-
   </div>
 </template>
 
-
-<script >
-import {findCategoryName, getCategory, upGoodsPhoto, IMG_URL,myGoodsEdit, myGoodsEditByIsViolation, } from "../../api/api";
+<script>
+import {
+  findCategoryName,
+  getCategory,
+  upGoodsPhoto,
+  IMG_URL,
+  myGoodsEdit,
+  myGoodsEditByIsViolation
+} from '../../api/api'
 
 export default {
-  name:"editGoods",
+  name: 'editGoods',
   data() {
     return {
-      title:"",
-      img_url: IMG_URL,//图片主机地址
+      title: '',
+      img_url: IMG_URL, //图片主机地址
       showPicker: false,
-      goodsCategory: [],//分类数据
-      category_children: [],//二级分类数据
+      goodsCategory: [], //分类数据
+      category_children: [], //二级分类数据
       activeKey: 0,
-      categoryName:"",
+      categoryName: '',
       form: {},
-      type: "",
+      type: ''
     }
   },
 
   methods: {
-
     //切换二级分类
     tab_item(id) {
-      let newCategory = this.goodsCategory.filter(item => item.id === id)
+      let newCategory = this.goodsCategory.filter((item) => item.id === id)
       // console.log(newCategory)
       this.category_children = newCategory[0].children
     },
-    goConfirm(id,name) {
-      this.categoryName = name;
-      this.form.goodsCategoryId = id;
-      this.showPicker = false;
+    goConfirm(id, name) {
+      this.categoryName = name
+      this.form.goodsCategoryId = id
+      this.showPicker = false
     },
-    goBack(){
-      this.showPicker = false;
+    goBack() {
+      this.showPicker = false
     },
 
-    priceValidator(val){
+    priceValidator(val) {
       return new Promise((resolve) => {
-        this.$toast.loading('验证中...');
+        this.$toast.loading('验证中...')
         setTimeout(() => {
-          this.$toast.clear();
+          this.$toast.clear()
           let isemail = /(^[0-9]\d*(\.\d{1,2})?$)|(^0(\.\d{1,2})?$)/.test(val)
           if (isemail) {
             this.isBut = false
-            resolve(true);
+            resolve(true)
           } else {
             this.isBut = true
-            resolve(false);
+            resolve(false)
           }
-        }, 1000);
-      });
-
+        }, 1000)
+      })
     },
 
-
     onClickLeft() {
-      this.$router.go(-1);
+      this.$router.go(-1)
     },
 
     //发布
     onSubmit() {
-     if(this.type === "1"){
-      myGoodsEdit(
-        this.form
-      ).then(res => {
-        if (res.code === '200') {
-          this.$toast.success(res.msg)
-          this.$router.push('/myGoods?type=1')
-        } else{
-          this.$toast.fail(res.msg)
-        }
-      })
-     }if(this.type === "3"){
-      myGoodsEditByIsViolation(
-        this.form
-      ).then(res => {
-        if (res.code === '200') {
-          this.$toast.success(res.msg)
-          this.$router.push('/myGoods?type=3')
-        } else{
-          this.$toast.fail(res.msg)
-        }
-      })
-     }
-
-
+      if (this.type === '1') {
+        myGoodsEdit(this.form).then((res) => {
+          if (res.code === '200') {
+            this.$toast.success(res.msg)
+            this.$router.replace('/myGoods?type=1')
+          } else {
+            this.$toast.fail(res.msg)
+          }
+        })
+      }
+      if (this.type === '3') {
+        myGoodsEditByIsViolation(this.form).then((res) => {
+          if (res.code === '200') {
+            this.$toast.success(res.msg)
+            this.$router.replace('/myGoods?type=3')
+          } else {
+            this.$toast.fail(res.msg)
+          }
+        })
+      }
     },
     //上传闲置图片
-    afterRead(file){
-      const forms = new FormData();
-      forms.append("file",file.file);
-      upGoodsPhoto(forms).then(res => {
-        if(res.code !== '200'){
-        this.$toast.fail(res.msg)
-      }
+    afterRead(file) {
+      const forms = new FormData()
+      forms.append('file', file.file)
+      upGoodsPhoto(forms).then((res) => {
+        if (res.code !== '200') {
+          this.$toast.fail(res.msg)
+        }
         this.form.photo = res.data
       })
-    },
-
+    }
   },
 
-   mounted() {
-    getCategory().then(res => {
+  mounted() {
+    getCategory().then((res) => {
       this.goodsCategory = res.data //初始化分类数据
-      this.tab_item(res.data[0].id)//初始化二级分类数据
+      this.tab_item(res.data[0].id) //初始化二级分类数据
     })
     this.form = this.$route.query.val
     this.title = this.$route.query.title
     this.type = this.$route.query.type
 
-    findCategoryName(this.form.goodsCategoryId).then(res => {
-       this.categoryName = res.data //获取闲置名称
-
+    findCategoryName(this.form.goodsCategoryId).then((res) => {
+      this.categoryName = res.data //获取闲置名称
     })
-  },
-
-
-
-
+  }
 }
 </script>
 
-
-
-
 <style lang="less" scoped>
-
 #releaseGoods_title {
   text-align: center;
 }
 .van-sidebar-item--select::before {
-    background-color: #409EFF;
+  background-color: #409eff;
 }
 #goBack_title {
   margin-top: 8px;
@@ -273,7 +270,4 @@ export default {
   margin: 20px auto;
   border-radius: 12px;
 }
-
-
-
 </style>
--- src/views/user/myGoods.vue
+++ src/views/user/myGoods.vue
@@ -1,43 +1,38 @@
-<!--我的闲置页面-->
+<!-- 我的闲置页面 -->
 <template>
   <div id="orderType">
-     <!--    顶部标题-->
+    <!-- 顶部标题 -->
     <van-sticky>
-      <!-- <TopTitle :t_name="title" :ificon="ificon"/> -->
       <van-nav-bar
-      :title= this.title
-      left-text="返回"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-    >
-      <template #right>
-        <van-icon name="wap-home" size="18" />
-      </template>
-    </van-nav-bar>
-
+        :title="this.title"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+      >
+        <template #right>
+          <van-icon name="wap-home" size="18" />
+        </template>
+      </van-nav-bar>
     </van-sticky>
-    <!--    顶部标题end-->
-    <!--    顶部标题end-->
+    <!-- 顶部标题 end -->
+    <!-- 顶部标题 end -->
     <van-tabs v-model="activetab">
       <van-tab title="我发布的" name="1">
-        <div  v-if="activetab === '1' "></div>
+        <div v-if="activetab === '1'"></div>
       </van-tab>
       <van-tab title="我卖出的" name="2">
-        <div  v-if="activetab === '2' "></div>
+        <div v-if="activetab === '2'"></div>
       </van-tab>
       <van-tab title="审核不通过" name="3">
-        <div  v-if="activetab === '3' "></div>
+        <div v-if="activetab === '3'"></div>
       </van-tab>
       <van-tab title="已下架" name="0">
-        <div v-if="activetab === '0' "></div>
+        <div v-if="activetab === '0'"></div>
       </van-tab>
     </van-tabs>
 
-
-
-
-    <van-cell-group v-for="val in goodsList" :key="goodsList.index" id="listbox">
+    <van-cell-group v-for="val in goodsList" :key="val.id" id="listbox">
       <!--
       val.createTime 订单创建时间
       val.statusDto.title 发货状态
@@ -45,7 +40,7 @@
       <!-- <van-cell :border="false" :title="val.createTime" size="large" /> -->
       <van-cell size="large">
         <template #default>
-          <!--            商品卡片-->
+          <!-- 商品卡片 -->
           <van-card
             id="shopCard"
             :price="val.sellPrice"
@@ -55,58 +50,123 @@
             :thumb="img_url + val.photo"
             class="goods-card"
           >
-          <template #tags>
-            <van-tag plain type="success" v-if="val.status === 1 && val.isVerify === 0">待审核</van-tag>
-            <van-tag plain type="danger" v-if="val.isViolation === 1 && val.isVerify === 1">审核不通过</van-tag>
-            <van-tag plain type="warning" v-if="val.isViolation === 1 && val.isVerify === 0">再次审核中</van-tag>
-            <van-tag plain type="primary" v-if="val.status === 2 ">卖出啦</van-tag>
-            <van-tag plain color="#909399" v-if="val.status === 0 ">已下架</van-tag>
-            <van-tag mark  type="danger" size="medium"  v-if="val.isViolation === 1">原因：</van-tag>
-            <van-tag round   color="#fff" size="medium" text-color="#000" v-if="val.isViolation === 1">{{ val.verifyContent }}</van-tag>
-          </template>
-          <template #footer>
-            <van-button size="mini" @click="Edit(val)" v-if="val.status === 1 ">编辑</van-button>
-            <van-button size="mini" @click="EditIsViolation(val)" v-if="val.isViolation === 1 && val.isVerify == 1">修改</van-button>
-            <van-button size="mini" @click="Sold(val.id)" v-if="val.status === 1 && val.isVerify === 1">确认售出</van-button>
-            <van-button size="mini" @click="Down(val.id)" v-if="val.status === 1 ">下架</van-button>
-            <van-button size="mini" @click="AgainUp(val.id)" v-if="val.status === 0 && val.isViolation === 0">重新上架</van-button>
-            <van-button size="mini" @click="Del(val.id)">删除</van-button>
-          </template>
+            <template #tags>
+              <van-tag
+                plain
+                type="success"
+                v-if="val.status === 1 && val.isVerify === 0"
+              >
+                待审核
+              </van-tag>
+              <van-tag
+                plain
+                type="danger"
+                v-if="val.isViolation === 1 && val.isVerify === 1"
+              >
+                审核不通过
+              </van-tag>
+              <van-tag
+                plain
+                type="warning"
+                v-if="val.isViolation === 1 && val.isVerify === 0"
+              >
+                再次审核中
+              </van-tag>
+              <van-tag plain type="primary" v-if="val.status === 2"
+                >卖出啦</van-tag
+              >
+              <van-tag plain color="#909399" v-if="val.status === 0"
+                >已下架</van-tag
+              >
+              <van-tag
+                mark
+                type="danger"
+                size="medium"
+                v-if="val.isViolation === 1"
+              >
+                原因：
+              </van-tag>
+              <van-tag
+                round
+                color="#fff"
+                size="medium"
+                text-color="#000"
+                v-if="val.isViolation === 1"
+              >
+                {{ val.verifyContent }}
+              </van-tag>
+            </template>
+            <template #footer>
+              <van-button size="mini" @click="Edit(val)" v-if="val.status === 1"
+                >编辑</van-button
+              >
+              <van-button
+                size="mini"
+                @click="EditIsViolation(val)"
+                v-if="val.isViolation === 1 && val.isVerify == 1"
+              >
+                修改
+              </van-button>
+              <van-button
+                size="mini"
+                @click="Sold(val.id)"
+                v-if="val.status === 1 && val.isVerify === 1"
+              >
+                确认售出
+              </van-button>
+              <van-button
+                size="mini"
+                @click="Down(val.id)"
+                v-if="val.status === 1"
+              >
+                下架
+              </van-button>
+              <van-button
+                size="mini"
+                @click="AgainUp(val.id)"
+                v-if="val.status === 0 && val.isViolation === 0"
+              >
+                重新上架
+              </van-button>
+              <van-button size="mini" @click="Del(val.id)"> 删除 </van-button>
+            </template>
             <!--            自定义右下角内容end-->
           </van-card>
           <!--            商品卡片end-->
         </template>
       </van-cell>
-
     </van-cell-group>
-
-
-
-
   </div>
 </template>
 
 <script>
-import TopTitle from "../../components/topTitle";
-import {getMyGoodsList, IMG_URL, myGoodsDel, myGoodsSold, myGoodsDown, myGoodsAgainUp} from '../../api/api'
+import TopTitle from '../../components/topTitle'
+import {
+  getMyGoodsList,
+  IMG_URL,
+  myGoodsDel,
+  myGoodsSold,
+  myGoodsDown,
+  myGoodsAgainUp
+} from '../../api/api'
 // import myGoodsList from "./myGoodsList";
 
 export default {
   name: 'myGoods',
   // components: {OrderTypeList, TopTitle},
   // components: {myGoodsList, TopTitle},
-  components: {TopTitle},
+  components: { TopTitle },
   data() {
     return {
       img_url: IMG_URL,
       title: '我的闲置',
       ificon: false,
-      activetab: '-1',//被选中的标签
+      activetab: '-1', // 被选中的标签
       numValue: '1',
-      goodsList: {},//闲置数据
-      //分页
-      limit: 10,//页大小,默认为 10
-      page: 1,//页码,默认为1
+      goodsList: [], // 闲置数据
+      // 分页
+      limit: 10, // 页大小,默认为 10
+      page: 1 // 页码,默认为1
     }
   },
   filters: {
@@ -114,7 +174,7 @@ export default {
     capittalizze(val) {
       let newVal = parseFloat(val).toFixed(2)
       return newVal
-    },
+    }
   },
   watch: {
     activetab(newVal, oldCal) {
@@ -124,103 +184,82 @@ export default {
 
   created() {
     this.getList()
-
   },
 
   methods: {
     getList(id) {
       getMyGoodsList({
         type: id,
-        limit: 10,//页大小,默认为 10
-        page: 1,//页码,默认为1
-      }).then(res => {
+        limit: 10, //页大小,默认为 10
+        page: 1 //页码,默认为1
+      }).then((res) => {
         // this.goodsList = res.data.records
         this.goodsList = res.data
-
       })
     },
-    updateWeb(){
+    updateWeb() {
       this.$router.push('/myGoods?type=1')
     },
 
-
-    Sold(id){
-      myGoodsSold(id).then(res => {
-        if(res.code !== '200'){
+    Sold(id) {
+      myGoodsSold(id).then((res) => {
+        if (res.code !== '200') {
           this.$toast.fail(res.msg)
         }
         this.$toast.success(res.msg)
-        this.$router.push('/mine')
-        // this.$router.push('/myGoods?type=2')
+        this.getList(this.activetab)
       })
     },
 
-    Down(id){
-      myGoodsDown(id).then(res => {
-        if(res.code !== '200'){
+    Down(id) {
+      myGoodsDown(id).then((res) => {
+        if (res.code !== '200') {
           this.$toast.fail(res.msg)
         }
         this.$toast.success(res.msg)
-        this.$router.push('/mine')
-      //   this.$router.push({
-      //   name: 'myGoods',
-      //   // sid: val
-      //   query: {
-      //     type: 0
-      //   }
-      // })
-
+        this.getList(this.activetab)
       })
     },
 
-    AgainUp(id){
-      myGoodsAgainUp(id).then(res => {
-        if(res.code !== '200'){
+    AgainUp(id) {
+      myGoodsAgainUp(id).then((res) => {
+        if (res.code !== '200') {
           this.$toast.fail(res.msg)
         }
         this.$toast.success(res.msg)
-        this.$router.push('/mine')
-
-        // this.$router.push('/myGoods?type=0')
-        // this.getList()
-
+        this.getList(this.activetab)
       })
     },
 
-    Del(id){
-      myGoodsDel(id).then(res => {
-        if(res.code !== '200'){
+    Del(id) {
+      myGoodsDel(id).then((res) => {
+        if (res.code !== '200') {
           this.$toast.fail(res.msg)
         }
         this.$toast.success(res.msg)
-        this.$router.push('/mine')
-        // this.updateWeb()
-        // this.getList(1)
+        this.getList(this.activetab)
       })
     },
-
-
-
 
     Edit(val) {
-      this.$router.push({ path: '/editGoods', query: { val: val, title: "编辑我的闲置", type: 1} })
+      this.$router.replace({
+        path: '/editGoods',
+        query: { val: val, title: '编辑我的闲置', type: 1 }
+      })
     },
     EditIsViolation(val) {
-      this.$router.push({ path: '/editGoods', query: { val: val, title: "修改我的不通过闲置", type: 3 } })
+      this.$router.push({
+        path: '/editGoods',
+        query: { val: val, title: '修改我的不通过闲置', type: 3 }
+      })
     },
 
     onClickLeft() {
-      this.$router.go(-1);
+      this.$router.go(-1)
     },
     onClickRight() {
-      this.$router.push( '/mine');
-    },
-
-
-
-
-
-
+      this.$router.push('/mine')
+    }
   },
   mounted() {
     let id = this.$route.query.type
@@ -255,9 +294,7 @@ export default {
   }
 }
 
-
 /deep/ .van-tabs__line {
-    background-color: #409EFF;
+  background-color: #409eff;
 }
-
 </style>
